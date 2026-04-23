4295efeeebf2a252e5a9a18fbd1fc174440df3de
Matheus Lessa <matheus.lessa.dev@gmail.com>
2026-04-22 21:01:10 -0300

    fix: DockerFiles e Compose, e Application.Properties modificados

---
+++ backend/src/main/java/com/saas_aldeia/backend/repository/AlunoRepository.java
@@ -0,0 +1,10 @@
+package com.saas_aldeia.backend.repository;
+
+
+import com.saas_aldeia.backend.model.Aluno; // importa sua Model
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+}

c4428631cd3557be74781a46a2630e1d28b11088
Matheus Lessa <matheus.lessa.dev@gmail.com>
2026-04-23 21:16:15 -0300

    Fix: Arrumado problema de integração com Docker

---
+++ backend/src/main/java/com/saas_aldeia/backend/service/AlunoService.java
@@ -0,0 +1,19 @@
+package com.saas_aldeia.backend.service;
+
+import com.saas_aldeia.backend.dto.AlunoRequest;
+import com.saas_aldeia.backend.dto.AlunoResponse;
+import com.saas_aldeia.backend.model.Aluno;
+import com.saas_aldeia.backend.repository.AlunoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AlunoService {
+
+    private final AlunoRepository alunoRepository;
+
+
+}

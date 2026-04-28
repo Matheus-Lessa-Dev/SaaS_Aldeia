2f31f8af045f410d608bb8d7420cd25b7abbe816
Matheus Lessa <matheus.lessa.dev@gmail.com>
2026-04-28 08:15:40 -0300

    Fix: Corrigindo as classes anteriores para dar certo com a classe usuario

--- backend/src/main/java/com/saas_aldeia/backend/model/Admin.java
+++ backend/src/main/java/com/saas_aldeia/backend/model/Admin.java
@@ -3,25 +3,18 @@ package com.saas_aldeia.backend.model;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "admin")
+@PrimaryKeyJoinColumn(name = "usuario_id")
 @Data
+@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
-public class Admin {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Admin extends Usuario {
 
     @Column(nullable = false, length = 45)
     private String nome;
-
-    @Column(nullable = false, length = 70, unique = true)
-    private String email;
-
-    @Column(nullable = false, length = 255)
-    private String senha;
 }
--- backend/src/main/java/com/saas_aldeia/backend/model/Aluno.java
+++ backend/src/main/java/com/saas_aldeia/backend/model/Aluno.java
@@ -3,31 +3,25 @@ package com.saas_aldeia.backend.model;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "aluno")
+@PrimaryKeyJoinColumn(name = "usuario_id")
 @Data
+@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
-public class Aluno {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Aluno extends Usuario {
 
     @Column(nullable = false, length = 45)
     private String nome;
 
     @Column(name = "data_nascimento")
     private LocalDate dataNascimento;
-
-    @Column(nullable = false, length = 75, unique = true)
-    private String email;
-
-    @Column(nullable = false, length = 255)
-    private String senha;
 
     @Column(length = 45)
     private String rua;

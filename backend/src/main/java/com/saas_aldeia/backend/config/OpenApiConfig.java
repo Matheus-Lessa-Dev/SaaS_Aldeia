d157554ec0a128ea51ca3c6d0ec3f565d4ec437e
Samuel <samuel.scardoso2004@gmail.com>
2026-06-09 15:27:41 -0300

    feat: autorização no swagger

--- backend/src/main/java/com/saas_aldeia/backend/config/OpenApiConfig.java
+++ backend/src/main/java/com/saas_aldeia/backend/config/OpenApiConfig.java
@@ -9,10 +9,18 @@ import io.swagger.v3.oas.models.OpenAPI;
 public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
                 .info(new io.swagger.v3.oas.models.info.Info()
                         .title("Aldeia API")
                         .description("API para gerenciamento de jogos e usuários")
-                        .version("1.0.0"));
+                        .version("1.0.0"))
+                .addSecurityItem(
+                        new io.swagger.v3.oas.models.security.SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new io.swagger.v3.oas.models.Components().addSecuritySchemes(securitySchemeName,
+                                new io.swagger.v3.oas.models.security.SecurityScheme().name(securitySchemeName)
+                                        .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                                        .scheme("bearer").bearerFormat("JWT")));
     }
 }

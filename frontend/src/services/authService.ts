087e812fe446c53471d9012692ef6fe2addef08f
Samuel <samuel.scardoso2004@gmail.com>
2026-06-20 20:03:58 -0300

    feat: id do user é salvo corretamente no login

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,29 +1,38 @@
-import api from './api';
-import { Role } from '../context/AuthContext';
+import api from "./api";
+import { Role } from "../context/AuthContext";
 
 interface LoginResponse {
   token: string;
   role: string;
   email: string;
   nome: string;
+  id: number;
 }
 
 export function mapRole(apiRole: string): Role {
   switch (apiRole) {
-    case 'ADMIN':     return Role.Admin;
-    case 'PROFESSOR': return Role.Teacher;
-    case 'ALUNO':     return Role.Student;
-    default: throw new Error(`Role desconhecido: ${apiRole}`);
+    case "ADMIN":
+      return Role.Admin;
+    case "PROFESSOR":
+      return Role.Teacher;
+    case "ALUNO":
+      return Role.Student;
+    default:
+      throw new Error(`Role desconhecido: ${apiRole}`);
   }
 }
 
 export async function loginRequest(email: string, senha: string) {
-  const { data } = await api.post<LoginResponse>('/auth/login', { email, senha });
+  const { data } = await api.post<LoginResponse>("/auth/login", {
+    email,
+    senha,
+  });
 
   return {
     token: data.token,
     email: data.email,
     nome: data.nome,
     role: mapRole(data.role),
+    id: data.id,
   };
 }
